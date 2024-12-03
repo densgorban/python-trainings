57e07f43290c284ba3604d5ec924f61a088f03d9
Denys Horban <dens.gorban@gmail.com>
2024-12-03 23:32:54 +0200

    reflection about functions and comments

--- general-trainings/functions.py
+++ general-trainings/functions.py
@@ -1,5 +1,18 @@
-def v(a):
+def v(a: object) -> object:
     print(a)
 
 
 v("Name")
+
+
+def sum_nums(a, b):
+    """
+      The doc...
+    @param a:numeric
+    @param b:numeric
+    @return:numeric
+    """
+    return a + b
+
+
+v(sum_nums(1, 5))
